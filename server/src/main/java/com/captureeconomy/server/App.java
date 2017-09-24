0acab72dec49e76f7a2a3b11cd7a97a47c91b9a8
Gavin Haynes <haynes.gt@gmail.com>
2017-09-24 16:13:47 -0700

    Add server and update pom files

---
+++ server/src/main/java/com/captureeconomy/server/App.java
@@ -0,0 +1,13 @@
+package com.captureeconomy.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
